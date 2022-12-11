f13d2ccc9b6bd99da1ced5642900471f71132938
Andreas Philippi <18646892+andreasphil@users.noreply.github.com>
2022-12-11 12:34:21 +0100

    feat: custom color scheme

--- utils/lib.tsx
+++ utils/lib.tsx
@@ -62,7 +62,6 @@ export function Layout(
       <Head>
         <title>{getPageTitle(props.title)}</title>
         <meta name="viewport" content="width=device-width,initial-scale=1" />
-        <meta name="theme-color" content="hsl(155 80% 38%)" />
         <link rel="icon" href="/favicon.ico" sizes="any" />
         <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
         <link rel="stylesheet" href="/style.css" />
